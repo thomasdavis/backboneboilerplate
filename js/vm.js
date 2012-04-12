18ebc184fbd1a40f76b78f94aabc81027ca72b04
Thomas Davis <thomasalwyndavis@gmail.com>
2012-04-12 23:14:57 +1000

    undelegate events

--- js/vm.js
+++ js/vm.js
@@ -9,7 +9,7 @@ define([
 	var create = function (context, name, View, options) {
 		// View clean up isn't actually implemented yet but will simply call .clean, .remove and .unbind
 		if(typeof views[name] !== 'undefined') {
-			views[name].unbind();
+			views[name].undelegateEvents();
 			if(typeof views[name].clean === 'function') {
 				views[name].clean();
 			}
