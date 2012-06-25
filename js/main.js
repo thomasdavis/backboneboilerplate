f15109b1d760b42be5227e72e273c44d73ad3882
Nietzsche <thomasalwyndavis@gmail.com>
2012-06-25 18:52:41 +1000

    updated r.js to work

--- js/main.js
+++ js/main.js
@@ -15,8 +15,7 @@ require.config({
     // Just a short cut so we can put our html outside the js dir
     // When you have HTML/CSS designers this aids in keeping them out of the js directory
     templates: '../templates'
-  },
-	urlArgs: "bust=" +  (new Date()).getTime()
+  }
 
 });
 
